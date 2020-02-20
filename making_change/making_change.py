774ee9c22143586494b003ae572e1c510ffc132a
BaoPham92 <baopham92@yahoo.com>
2020-02-20 17:36:56 -0500

    Eating cookies && Making change first pass

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,9 +2,29 @@
 
 import sys
 
-def making_change(amount, denominations):
-  pass 
+def making_change(amount, denominations = []):
 
+  # * FLOAT INPUT AMOUNT
+  amount = float(amount)
+  print(f'Amount: {amount} Denominations: {denominations}')
+
+  # * PLACEHOLDER FOR VALID CONVERSIONS
+  available_conversions = 0
+  print(f'Current Conversions: {available_conversions}')
+
+  # * IF THERE ARE ARRAY OF DENOMINATIONS IS NOT EMPTY
+  if (len(denominations) > 0):
+    
+    # * CALCULATE NUMBER OF POSSIBLE CONVERSIONS
+    for denominator in denominations:
+      division = float(amount / denominator)
+
+      # ? IF TRUE, INCREMENT POSSIBLE CONVERSIONS
+      if (division * denominator == amount): 
+        available_conversions += 1
+
+  print(f'{available_conversions}')
+  return available_conversions
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
@@ -15,3 +35,6 @@ if __name__ == "__main__":
     print("There are {ways} ways to make {amount} cents.".format(ways=making_change(amount, denominations), amount=amount))
   else:
     print("Usage: making_change.py [amount]")
+
+  making_change(20, [0.01, 0.05, 0.10, 0.25, 0.50])
+  making_change(350, [1, 5, 10, 25, 50])
